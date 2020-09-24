f1f1e15b13a5ccad1ea4232acc47877d65d062d7
Terez Julianna Szabo <szabo_terez@student.ceu.edu>
2020-09-24 19:42:13 +0200

    created szabo_terez and nano file for script

---
+++ szabo_terez/IT/my_script.sh
@@ -0,0 +1,3 @@
+mkdir made_by_my_script > szabo_terez/IT
+Echo "Hello World" > szabo_terez/IT/made_by_my_script/hellp.txt
+ls szabo_terez/IT/made_by_my_script/ > szabo_terez/IT/made_by_my_script/list_of_files.txt

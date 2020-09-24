219297f3055d2f0531690696f1472d7666c27c29
Terez Julianna Szabo <szabo_terez@student.ceu.edu>
2020-09-24 19:49:53 +0200

    added shell script my_script

--- szabo_terez/IT/my_script.sh
+++ szabo_terez/IT/my_script.sh
@@ -1,3 +1,3 @@
-mkdir made_by_my_script > szabo_terez/IT
-Echo "Hello World" > szabo_terez/IT/made_by_my_script/hellp.txt
-ls szabo_terez/IT/made_by_my_script/ > szabo_terez/IT/made_by_my_script/list_of_files.txt
+mkdir made_by_my_script
+Echo "Hello World" > made_by_my_script/hellop.txt
+ls made_by_my_script/ > made_by_my_script/list_of_files.txt

4f66c97153a214e2208e0fc74edad31c1f754c7a
Terez Julianna Szabo <szabo_terez@student.ceu.edu>
2020-09-24 19:55:11 +0200

    folder and files after running my_scirpt

--- szabo_terez/IT/my_script.sh
+++ szabo_terez/IT/my_script.sh
@@ -1,3 +1,3 @@
 mkdir made_by_my_script
-Echo "Hello World" > made_by_my_script/hellop.txt
+Echo "Hello World" > made_by_my_script/hello.txt
 ls made_by_my_script/ > made_by_my_script/list_of_files.txt
